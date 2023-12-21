b7331261d0dfe1820ede45bd1e44e14f79260eb0
Andrew Koval <andrewkovalsotheremail@gmail.com>
2023-12-21 18:29:53 +0000

    Mongo lab Error Handling

--- tasky-api/api/tasks/taskModel.js
+++ tasky-api/api/tasks/taskModel.js
@@ -7,9 +7,14 @@ const TaskSchema = new Schema({
   description:  String ,
   deadline: Date,
     done: Boolean,
-    priority: {type: String, enum: ["Low","Medium","High"]},
+    priority: { type: String, enum: ["Low", "Medium", "High"], required: true },
     created_at: Date,
     updated_at: Date
 });
+
+const dateValidator = (date) => {
+  return date > new Date();
+}
+TaskSchema.path("deadline").validate(dateValidator);
 
 export default mongoose.model('Task', TaskSchema);
