67d04bca509a92d7bfe65f6a6652b41e4c51a5b7
AndrewK0val <andrewkoval2003@gmail.com>
2023-12-22 16:44:52 +0000

    Added week 11 exercise (genres)

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -13,5 +13,21 @@ export const getUpcomingMovies = async () => {
         return await response.json();
     } catch (error) {
         throw error;
-    }
+    } 
 };
+
+
+export const getGenres = async () => {
+    try {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.TMDB_KEY}&language=en`
+        );
+
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }};
